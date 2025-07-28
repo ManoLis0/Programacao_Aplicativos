22497bf2efcd7da66c94e926908bd62e7fbb5e36
Afonso <piazzetta.afonso@gmail.com>
2025-07-28 09:48:30 -0300

    segunda commit

---
+++ Aula0.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Digite o seu nome: ");
+let idade = prompt("Digite o sua idade: ");
+console.log("Bem Vindo", nome, "Voce tem", idade, "anos");
